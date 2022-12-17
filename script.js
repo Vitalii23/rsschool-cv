45ae4dc87bcca29d5e274c1ca3ea59333f90e6f4
Vitalii Danilov <Vitalii Danilov@DESKTOP-GIENFQV>
2022-12-17 16:35:25 +0700

    feat: refactory code and well done task

--- script.js
+++
@@ -1,6 +0,0 @@
-(function () {
-    let header = document.querySelector('h2');
-    for (let i = 0; i < header.length; i++){
-        header[i].setAttribute('data-content', header[i].innerHTML);
-    }
-})();
